a6d4c85c39d59b81997a14690a08099ea56e3c5a
devin <942668138@qq.com>
2020-12-24 13:15:24 +0800

    1. 郭峻溪 2. 增加订单类的ID

--- src/main/java/com/devin/entity/Orders.java
+++ src/main/java/com/devin/entity/Orders.java
@@ -3,6 +3,11 @@ package com.devin.entity;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.io.Serializable;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -22,6 +27,13 @@ import lombok.EqualsAndHashCode;
 public class Orders implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @ApiModelProperty(value = "id")
+    // 雪花算法，保证id唯一
+    @TableId(type = IdType.ASSIGN_ID)
+    // 字符串序列化
+    @JsonSerialize(using = ToStringSerializer.class)
+    private Long id;
 
     @ApiModelProperty(value = "所属用户的id")
     private Long userId;
